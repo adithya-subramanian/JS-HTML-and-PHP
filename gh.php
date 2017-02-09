ac369891bac0cc118b4611a41c971fbe358d1072
adithya-subramanian <adisubbu86@gmail.com>
2017-02-09 12:58:13 +0530

    Add files via upload

---
+++ gh.php
@@ -0,0 +1,25 @@
+<?php
+function a($v)
+{
+while($a)
+{
+//kuch bhi like de bc 
+}
+echo $j;
+}
+function b($v)
+{
+echo $v + 20;
+}
+$r = 1;
+if($r == 1)
+{
+$r = 'a';
+$r();
+}
+else
+{
+$r = 'b'
+$r();
+}
+?>
---
+++ ui.php
@@ -0,0 +1,11 @@
+<?php
+function a($v)
+{
+for($i = 0;$i  < count($a);$i++)
+{
+$a[$i] = $a[$i] + 2;
+}
+}
+$v = array(1,2,3,4,5);
+abc($V);
+?>
