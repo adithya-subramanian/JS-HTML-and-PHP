1d936ef3422c864b80717976bd6afae9fda022a8
adithya-subramanian <adisubbu86@gmail.com>
2017-02-10 12:35:46 +0530

    Update class2_php.php

--- class2_php.php
+++ class2_php.php
@@ -2,9 +2,15 @@ $k = array(array('name'=>'arjit','marks'=>66),array('name'=>'sethi','marks'=>99)
 $max = 101;
 for($i = 0;$i < count($k);$i++)
 {
+foreach($k[$i] as $key => $value)
+{
+if($key == 'marks')
+{
 if($max > $k[$i]['marks'])
 {
 $max = $k[$i]['marks'];
+}
+}
 }
 }
 echo $max;
